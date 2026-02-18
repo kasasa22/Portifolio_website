e5c5ee3088d8947f3d3d6a9ff579ae821ef4068a
kasasa22 <kasasatrevor25@gmail.com>
2026-02-18 10:18:26 +0300

    feat: Add React Router with animated page transitions

--- src/App.js
+++ src/App.js
@@ -1,55 +1,43 @@
-import React from 'react';
 import styled, { ThemeProvider } from "styled-components";
 import { darkTheme } from "./utils/Themes";
 import Navbar from "./components/Navbar";
-import { BrowserRouter } from "react-router-dom";
-import Hero from "./components/sections/Hero";
-import Skills from './components/sections/Skills';
-import Experience from './components/sections/Experience';
-import Projects from "./components/sections/Projects";
-import Education from "./components/sections/Education";
-import Contact from "./components/sections/Contact";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import Home from "./pages/Home";
+import ProjectDetail from "./pages/ProjectDetail";
+import AllProjects from "./pages/AllProjects";
+import ScrollToTop from "./components/ScrollToTop";
 
 const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
   width: 100%;
-  height: 98vh;
+  min-height: 100vh;
   overflow-x: hidden;
   position: relative;
 `;
-const Wrapper = styled.div`
-  padding-bottom: 100px;
-  background: linear-gradient(
-      38.73deg,
-      rgba(204, 0, 187, 0.15) 0%,
-      rgba(201, 32, 184, 0) 50%
-    ),
-    linear-gradient(
-      141.27deg,
-      rgba(0, 70, 209, 0) 50%,
-      rgba(0, 70, 209, 0.15) 100%
-    );
-  width: 100%;
-  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
-`;
+
+const AnimatedRoutes = () => {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<AllProjects />} />
+        <Route path="/projects/:id" element={<ProjectDetail />} />
+      </Routes>
+    </AnimatePresence>
+  );
+};
 
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <BrowserRouter>
+        <ScrollToTop />
         <Navbar />
         <Body>
-          <Hero/>
-          <Wrapper>
-              <Skills />
-              <Experience />
-            </Wrapper>
-            <Projects />
-            <Wrapper>
-              <Education />
-              <Contact />
-            </Wrapper>
-
+          <AnimatedRoutes />
         </Body>
       </BrowserRouter>
     </ThemeProvider>
